2de42030454f7f17e5ac7200b58c45804475a9ef
Saliha Kotan <salihakotan77@gmail.com>
2024-11-25 10:22:15 +0300

    user model upd

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    full_name: { type: String },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
